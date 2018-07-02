3ad7dd076fcad65ec8ac123f7da4bc342d25f793
shindk8659 <shindk8659@naver.com>
2018-07-02 15:48:10 +0900

    add kakaoAPI Login

--- FoodChain_iOS/ViewController/Main/LoginViewController.swift
+++ FoodChain_iOS/ViewController/Main/LoginViewController.swift
@@ -21,21 +21,51 @@ class LoginViewController: UIViewController {
 
     @IBAction func LoginAction(_ sender: Any) {
         
-        let mainview = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "tabbarview") as! TabBarViewController
+//        let mainview = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "tabbarview") as! TabBarViewController
+//
+//        present(mainview, animated: true, completion: nil
+//        )
         
-        present(mainview, animated: true, completion: nil
-        )
+        let session :KOSession = KOSession.shared()
+        
+        if session.isOpen() {
+            session.close()
+        }
+        
+        session.presentingViewController = self
+        session.open(completionHandler: {(error) -> Void in
+            
+            // 카카오 로그인 화면에서 벋어날 시 호출됨. (취소일 때도 표시됨)
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+            }else if session.isOpen() {
+                KOSessionTask.userMeTask(completion: { (profile, error)  -> Void in
+                    if profile != nil{
+                        print(profile!)
+                    }
+                    else{
+                        print(error!)
+                    }
+                })
+            
+            } else {
+                print("isNotOpen")
+            }
+        })
+        
+        
+       
        
     }
     
-    @IBAction func ContinueAction(_ sender: Any) {
-        
-        let favoriteview = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "favoriteview") 
-        present(favoriteview, animated: true, completion: nil)
-        
-        
-        
-    }
+//    @IBAction func ContinueAction(_ sender: Any) {
+//
+//        let favoriteview = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "favoriteview")
+//        present(favoriteview, animated: true, completion: nil)
+//
+//
+//
+//    }
     
 
 }
